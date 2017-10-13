982be8b734c56b134c7c92fb809739962ee0d396
Roman Roelofsen <romanroe@gmail.com>
2017-10-13 16:32:24 +0200

    added npm package to readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+
+[![NPM](https://nodei.co/npm/tydux.png)](https://npmjs.org/package/@w11k/tydux)
+
 [![Build Status](https://travis-ci.org/Tydux/Tydux.svg?branch=master)](https://travis-ci.org/Tydux/Tydux)
 
 # Tydux
--- dev/publish.sh
+++ dev/publish.sh
@@ -5,12 +5,11 @@ cd ..
 
 git add .
 
-PACKAGE_VERSION=$(cat package.json  | grep version  | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+#PACKAGE_VERSION=$(cat package.json  | grep version  | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 
-
-git commit -m "new release v$PACKAGE_VERSION"
-git tag v${PACKAGE_VERSION}
 
 npm run dist && npm run test && npm publish --access public
 npm version patch
 git push
+git push --tags
+
