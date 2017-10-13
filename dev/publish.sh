79859e5cbee3480280505580f94653a8f62a30eb
Roman Roelofsen <romanroe@gmail.com>
2017-10-13 16:26:39 +0200

    added git tag to release process

--- dev/publish.sh
+++ dev/publish.sh
@@ -4,7 +4,13 @@ cd `dirname $0`
 cd ..
 
 git add .
-git commit -m "new release"
+
+PACKAGE_VERSION=$(cat package.json  | grep version  | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+
+
+git commit -m "new release v$PACKAGE_VERSION"
+git tag v${PACKAGE_VERSION}
+
 npm run dist && npm run test && npm publish --access public
 npm version patch
 git push
