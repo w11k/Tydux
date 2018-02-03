4a5de93cb04689a9f062341e301b573491972c97
Roman Roelofsen <romanroe@gmail.com>
2018-02-03 16:42:39 +0100

    new release 1.3.0

--- dev/publish.sh
+++ dev/publish.sh
@@ -3,12 +3,15 @@
 cd `dirname $0`
 cd ..
 
-git add .
-
+PACKAGE_VERSION=v$(cat package.json  | grep version  | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 
 npm run dist && \
 npm run test && \
-npm version patch && \
+git add . \
+git commit -m "new release ${PACKAGE_VERSION}" \
+git tag ${PACKAGE_VERSION} \
 npm publish --access public && \
 git push && \
-git push --tags
+git push --tags \
+npm --no-git-tag-version version patch && \
+git push
