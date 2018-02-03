d81b3d05d20cede5b05b1e8f0ff56f27c7c83060
Roman Roelofsen <romanroe@gmail.com>
2018-02-03 16:33:45 +0100

    changed publish script

--- dev/publish.sh
+++ dev/publish.sh
@@ -5,11 +5,10 @@ cd ..
 
 git add .
 
-#PACKAGE_VERSION=$(cat package.json  | grep version  | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 
-
-npm run dist && npm run test && npm publish --access public
-npm version patch
-git push
+npm run dist && \
+npm run test && \
+npm version patch && \
+npm publish --access public && \
+git push && \
 git push --tags
-
