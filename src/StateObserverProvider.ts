b1e357ad067c6215d8b6f0f7cccc7bc09fea5426
Roman Roelofsen <romanroe@gmail.com>
2018-04-20 16:41:34 +0200

    added `state` property to `StateObserverProvider`

--- src/StateObserverProvider.ts
+++ src/StateObserverProvider.ts
@@ -3,6 +3,8 @@ import {StateObserver} from "./StateObserver";
 
 export interface StateObserverProvider<S> {
 
+    state: Readonly<S>;
+
     bounded(operator: Operator<S, S>): StateObserver<S>;
 
     unbounded(): StateObserver<S>;
