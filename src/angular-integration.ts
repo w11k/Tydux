2a72b4300776f54cd0e44ebcef1d7311ac63cc7b
Roman Roelofsen <romanroe@gmail.com>
2018-11-25 13:58:31 +0100

    - cleanup

--- src/angular-integration.ts
+++ src/angular-integration.ts
@@ -36,38 +36,3 @@ export function toAngularComponent<T>(component: OnDestroyLike): Operator<T, T> 
     );
 }
 
-// export function provideTydux<S>(initialState: S, enhancer?: StoreEnhancer<any>): { provide: typeof TyduxStore, useValue: TyduxStore<S> } {
-//     return {
-//         provide: TyduxStore,
-//         useValue: createTyduxStore(initialState, enhancer)
-//     };
-// }
-//
-// export type Provider = typeof TyduxReducerBridge | { provide: any, useFactory: Function, deps: any[] };
-//
-// export const STORE = "TyduxStoreFactoryToken";
-//
-// export function provideTyduxWithStoreFactory(storeFactory: (bridge: TyduxReducerBridge) => Store, storeToken: any = STORE): Provider[] {
-//     return [
-//         TyduxReducerBridge,
-//         {
-//             provide: storeToken,
-//             useFactory: storeFactory,
-//             deps: [TyduxReducerBridge]
-//         },
-//         {
-//             provide: TyduxStore,
-//             useFactory: (bridge: TyduxReducerBridge, store: Store) => {
-//                 return bridge.connectStore(store);
-//             },
-//             deps: [TyduxReducerBridge, storeToken]
-//         },
-//     ]
-// }
-//
-
-
-
-
-
-
