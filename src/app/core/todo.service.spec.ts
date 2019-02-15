3ecc631d09060ae4c88f9c69af5fb3a45b7f5fa7
Sascha Engmann <engmann@w11k.de>
2019-02-15 14:27:11 +0100

    WIP Workaround of jest bug

--- src/app/core/todo.service.spec.ts
+++ src/app/core/todo.service.spec.ts
@@ -1,14 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TodoService } from './todo.service';
-import { TyduxStore } from '@w11k/tydux';
+import { TyduxModule } from '../../../projects/w11k/tydux-angular/src/public_api';
 
 describe('TodoService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      { provide: TyduxStore, useValue: {} }
-    ]
-  }));
+  beforeEach(() => {
+    console.log(TyduxModule);
+    TestBed.configureTestingModule({
+      imports: [
+        TyduxModule.forRoot()
+      ]
+    })
+  });
 
   it('should be created', () => {
     const service: TodoService = TestBed.get(TodoService);
--- src/app/todo-list-context/todo-list-context.component.spec.ts
+++ src/app/todo-list-context/todo-list-context.component.spec.ts
@@ -3,7 +3,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TodoListContextComponent } from './todo-list-context.component';
 import { MockComponent } from 'ng-mocks';
 import { TodoListComponent } from '../todo-list/todo-list.component';
-import { TyduxStore } from '../../../projects/w11k/tydux/src/lib/store';
 
 describe('TodoListContextComponent', () => {
   let component: TodoListContextComponent;
@@ -15,9 +14,6 @@ describe('TodoListContextComponent', () => {
         TodoListContextComponent,
         MockComponent(TodoListComponent)
       ],
-      providers: [
-        { provide: TyduxStore, useValue: {} }
-      ]
     })
     .compileComponents();
   }));
