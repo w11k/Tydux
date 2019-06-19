a4a5af5f7ee4d40e02c9ba519f35c311d55c542a
Roman Roelofsen <romanroe@gmail.com>
2019-06-19 08:56:49 +0200

    deployed version v11.2.0

---
+++ dev/publish.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+cd `dirname $0`/..
+
+cd projects/w11k/tydux
+PACKAGE_VERSION=v$(cat package.json  | grep version  | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+cd ../../..
+
+git checkout master && \
+git fetch && \
+git merge origin/master && \
+rm -rf node_modules && \
+rm package-lock.json && \
+npm install && \
+npm run build:lib  && \
+npm run test:all && \
+git tag ${PACKAGE_VERSION} && \
+git add . && \
+git commit -m "deployed version ${PACKAGE_VERSION}" && \
+git push && \
+git push --tags && \
+cd dist/w11k/tydux && \
+npm publish --access public && \
+cd ../../..
+
+#git push
+#npm --no-git-tag-version version patch
--- projects/w11k/tydux/README.md
+++
@@ -1,4 +0,0 @@
-
-# Tydux
-
-## See️ [Tydux GitHub Readme](https://github.com/w11k/tydux)
