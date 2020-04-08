88b78693a1043a160f55d68b3ab1f6bd0c64c082
Roman Roelofsen <romanroe@gmail.com>
2020-04-08 16:13:17 +0200

    added mutators to public API

--- projects/w11k/tydux/src/public_api.ts
+++ projects/w11k/tydux/src/public_api.ts
@@ -9,6 +9,7 @@ export * from "./lib/store";
 export * from "./lib/store-global";
 export * from "./lib/utils";
 export * from "./lib/ordered-map.facade";
+export * from "./lib/commands-mutators";
 
 // TODO separate entrypoint for testing support
 export * from "./testing";
